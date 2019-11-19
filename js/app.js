2efbf8460e0e23dbb93b4ec96f8a426e6035c9bc
yong <yong.southa@gmail.com>
2019-11-19 14:42:28 +0100

    comments

--- js/app.js
+++ js/app.js
@@ -1,29 +1,39 @@
+//Créer les fonctionnalité dynamique de l'application
+
+// Création d'une variable saisie par l'utilisateurs (à convertir)
+// Création d'une variable sortie par l'application (convertit)
 const input = document.getElementById('lbsInput');
 const output = document.getElementById('output');
 
+// Ne aps afficher la conversation avant que l'utilisateurs est saisie une valeur
 output.style.visibility = 'hidden';
 
+//Crée un événement à suivre : cet événement est la saisie d'une valeurs par l'utilisateurs
 input.addEventListener('input', (e) => {
   let lbs = e.target.value;
 
+  // SI saisie, alors les convertions deviennent visible
   output.style.visibility = 'visible';
 
+  //Convertion en gr
   let grams = document.getElementById('gOutput');
-  grams.innerHTML = lbs/0.0022046;
+  grams.innerHTML = lbs / 0.0022046;
 
+  //Convertion en Kg
   let kiloGrams = document.getElementById('kgOutput');
-  kiloGrams.innerHTML = lbs/2.2046;
+  kiloGrams.innerHTML = lbs / 2.2046;
 
+  //Convertion en Ounces
   let ounces = document.getElementById('ozOutput');
-  ounces.innerHTML = lbs*16;
+  ounces.innerHTML = lbs * 16;
 
   e.preventDefault();
 });
 
-
+//Vérifier l'état de marche du service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('../sw.js').then( () => {
+    navigator.serviceWorker.register('../sw.js').then(() => {
       console.log('Service Worker Registered')
     })
   })
--- sw.js
+++ sw.js
@@ -1,14 +1,24 @@
+//Création du service worker, permettant la mise en cache de nos fichier et donc l'accès à l'application hors ligne
+//Implémentation de la bibliothèque de service workbox
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js");
 
+//Vérifié la bonne implémentation
 if (workbox) {
-    console.log("ça marche !!");
+    console.log("Yay! Workbox is loaded !");
+    //fonction lançant le caching des fichiers
     workbox.precaching.precacheAndRoute([]);
 
-    workbox.routing.registerRoute(
+    /*  Mise en cache de fichier png, gif et jpg et édition du fichier sw-config.js 
+    */
+    workbox.routing.registerRoute(nb), (
+        //sélectionne le type de fichier à mettre en cache
         /(.*)others(.*)\.(?:png|gif|jpg)/,
+        //créer une variable contenant les fichiers mise en cache
+        //CacheFirst --> stratégie de cache : si un cache existe alors chargé le cache sans utilisié le réseaux
         new workbox.strategies.CacheFirst({
-            cacheName: "images",
+            cacheName: "images", //nom du cache
             plugins: [
+                //gérer le nombre et l'age maximum des éléments dans le cache
                 new workbox.expiration.Plugin({
                     maxEntries: 50,
                     maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
@@ -16,21 +26,28 @@ if (workbox) {
             ]
         })
     );
-
+    /* Mise en cache des fichier JS et css et être sur de leurs mise à jours pour les prochaines utilisations. 
+    */
     workbox.routing.registerRoute(
+        //sélectionne le type de fichier à mettre en cache
         /.*\.(?:css|js|scss|)/,
+        //StaleWhileRevalidate --> stratégie de cache qui permet de répondre à la demande le plus rapidement avec une réponse en cache
+        // Si pas de réponse : requête réseau pour maj du cache 
         new workbox.strategies.StaleWhileRevalidate({
-            // use a custom cache name
+            // Indiquer le cache utiliser par la fonction
             cacheName: "assets",
         })
     );
 
-
+    // Mise en cache des polices
     workbox.routing.registerRoute(
         new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+        //CacheFirst --> stratégie de cache : si un cache existe alors chargé le cache sans utilisié le réseaux
         new workbox.strategies.CacheFirst({
+            // Indiquer le cache utiliser par la fonction
             cacheName: "google-fonts",
             plugins: [
+                //Choisir les demandes de caches répondant aux statuts préciser)
                 new workbox.cacheableResponse.Plugin({
                     statuses: [0, 200],
                 }),
@@ -38,13 +55,12 @@ if (workbox) {
         })
     );
 
-
-    workbox.googleAnalytics.initialize();
-
-
+    //Permet au service worker de mettre à jours les fichier caches sans tenir compte du cycle de vie
+    // Habituelle d'un service worker
     workbox.core.skipWaiting();
     workbox.core.clientsClaim();
 
+    // Si ça ne marche pas :
 } else {
     console.log("ça marche pas");
 }
